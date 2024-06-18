f7762d137dd5c51af936d96c1fad42f917709ef2
syllablelous <rapi.russeljerome.gratil@gmail.com>
2024-06-18 19:33:19 +0800

    Added delete & feature/unfeature functionality for admin wall, needs fix for refreshing the wall. Also some minor changes: Added featured variable to posts, and accounted for no featured post to appear on featured wall.

--- scripts/data/posts.js
+++ scripts/data/posts.js
@@ -11,24 +11,8 @@ export let posts = JSON.parse(localStorage.getItem('posts')) || [{
   featured: false // By default, posts are not featured unless inputted otherwise
 }];
 
-export let featured_posts = JSON.parse(localStorage.getItem('featured_posts')) || [{
-  postId: '2',
-  author: 'Neo',
-  title: 'Featured Title',
-  message: 'anofwaefawefawefsawnfoaefkanwefknaskdnflkanweklfnklasdnflkanwklefnklandsklfnaklwdnfklanwdklfnawklndfkawawefkwemfkanwekfnkawfwafewaawfeawakefnkajwnefawknefknawekfnkawenfkawnfwakefmkwafnkwaefnkawnefkaweekewnwaelfawlmefwlmeflawmflemwflaemflawwaefawefawfeawes',
-  theme: 'rgb(99, 211, 130)',
-  topic: 'images/technology.png',
-  time: 'June 1, 2024',
-  profilePicture: null,
-  comments: [1, 2]
-}];
-
 export function saveToStorage() {
   localStorage.setItem('posts', JSON.stringify(posts));
-}
-
-export function saveToFeatured() {
-  localStorage.setItem('featured_posts', JSON.stringify(featured_posts));
 }
 
 export function addPost(postId, author, title, message, theme, topic, time, profilePicture, featured) {
@@ -67,11 +51,6 @@ export function addCommentToPost(postId, commentId){
 
 // For deleting posts
 export function deletePostByPostId(postId){
-  // posts.forEach((post)=>{
-  //   if(post.postId == postId){
-  //     posts.splice(posts.indexOf(post),1);
-  //   }
-  // });
   // Dealing with modifying the length of the array, it's apparently recommended to iterate over the array backwards.
   for (let i = posts.length - 1; i >= 0; i--) {
     if (posts[i].postId == postId) {
@@ -85,9 +64,7 @@ export function deletePostByPostId(postId){
 export function setFeatureToPost(postId){
   posts.forEach((post)=>{
     if(post.postId == postId){
-      // post.featured = post.featured === "true" ? "false" : "true";
       post.featured = !post.featured;
-      console.log(postId + post.featured);
     }
   });
   saveToStorage();
