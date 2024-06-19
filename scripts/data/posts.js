31f0021ef59d063ea5e3ea08debddff6d42e91bf
Richtofenn <davidneo2040@gmail.com>
2024-06-20 01:05:03 +0800

    Add sample data for the posts and remove sample data on the submissions

--- scripts/data/posts.js
+++ scripts/data/posts.js
@@ -1,13 +1,68 @@
 export let posts = JSON.parse(localStorage.getItem('posts')) || [{
   postId: '201',
-  author: 'Neo',
-  title: 'Registrar',
-  message: 'Gaano po katagal i-process ang COR',
-  theme: 'rgb(99, 211, 130)',
+  author: 'ANONYMOUS PARTICIPANT',
+  title: 'NUIS',
+  message: 'Hello may di po ako na-OTE na dalawang subject. Ano po gagawin uli pag di nakapag-OTE na complete huhu😢',
+  theme: 'rgb(27, 49, 91)',
   topic: 'images/technology.png',
-  time: 'June 1, 2024',
+  time: 'June 18, 2024',
   profilePicture: 'images/bulldog.jpeg',
-  comments: [1, 2],
+  comments: [],
+  featured: false // By default, posts are not featured unless inputted otherwise
+}, {
+  postId: '202',
+  author: 'ANONYMOUS PARTICIPANT',
+  title: 'NU MOA',
+  message: 'Hello! Mag aask lang if may shs po ba sa nu moa?',
+  theme: 'rgb(216, 198, 35)',
+  topic: 'images/technology.png',
+  time: 'June 19, 2024',
+  profilePicture: 'images/bulldog.jpeg',
+  comments: [],
+  featured: false // By default, posts are not featured unless inputted otherwise
+}, {
+  postId: '203',
+  author: 'ANONYMOUS PARTICIPANT',
+  title: 'Accountancy uniform',
+  message: 'GOOD DAYYY! hm kaya yung uniform pang-accountancy',
+  theme: 'rgb(67, 177, 98)',
+  topic: 'images/technology.png',
+  time: 'June 20, 2024',
+  profilePicture: 'images/bulldog.jpeg',
+  comments: [],
+  featured: false // By default, posts are not featured unless inputted otherwise
+}, {
+  postId: '204',
+  author: 'ANONYMOUS PARTICIPANT',
+  title: 'OTE Reminder',
+  message: 'This is a gentle reminder that TODAY (June 19, 2024) is the last day for the OTE. Failure to do so may result in restrictions on certain applications within NUIS.',
+  theme: 'rgb(211, 99, 99)',
+  topic: 'images/technology.png',
+  time: 'June 20, 2024',
+  profilePicture: 'images/bulldog.jpeg',
+  comments: [],
+  featured: false // By default, posts are not featured unless inputted otherwise
+}, {
+  postId: '205',
+  author: 'ANONYMOUS PARTICIPANT',
+  title: 'NSTP department',
+  message: 'Hello bulldogs, saan na floor yung NSTP department? Tysia!🫶🫶',
+  theme: 'rgb(211, 99, 99)',
+  topic: 'images/technology.png',
+  time: 'June 20, 2024',
+  profilePicture: 'images/bulldog.jpeg',
+  comments: [],
+  featured: false // By default, posts are not featured unless inputted otherwise
+},{
+  postId: '265',
+  author: 'ANONYMOUS PARTICIPANT',
+  title: 'biochem',
+  message: 'hello po! ano po pwede ma-expect sa biochem? ano pong mahirap na part sa biochem?',
+  theme: 'rgb(216, 198, 35))',
+  topic: 'images/literature.png',
+  time: 'June 20, 2024',
+  profilePicture: 'images/bulldog.jpeg',
+  comments: [],
   featured: false // By default, posts are not featured unless inputted otherwise
 }];
 
